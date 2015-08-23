6fa959a59466d8592b262afc561f8f9522bb8751
Mathieu <mathieu@live.co.uk>
2015-08-23 15:35:50 +0200

    Multi lines statement extraction

--- Log4J2SLF4J.py
+++ Log4J2SLF4J.py
@@ -2,8 +2,8 @@ import re
 
 __author__ = 'mathieu'
 
-fileToProcess = "/Users/mathieu/Dvpt/workspace/voltage/voltage-commons/voltage-commons-utils/src/main/java/com/francetelecom/voltage/technicalservice/util/PieceJointe.java"
-#fileToProcess = "/Users/mathieu/Dvpt/workspace/voltage/voltage-core/voltage-core-business/src/main/java/com/francetelecom/voltage/business/demande/requisition/impl/ManageDemandeBusinessImpl.java"
+# fileToProcess = "/Users/mathieu/Dvpt/workspace/voltage/voltage-commons/voltage-commons-utils/src/main/java/com/francetelecom/voltage/technicalservice/util/PieceJointe.java"
+fileToProcess = "/Users/mathieu/Dvpt/workspace/voltage/voltage-core/voltage-core-business/src/main/java/com/francetelecom/voltage/business/demande/requisition/impl/ManageDemandeBusinessImpl.java"
 
 
 importStatementPattern = re.compile(r'\s*import\s*org\.apache\.log4j\.Logger\s*;\s*')
@@ -26,20 +26,50 @@ def getLines(filePath):
     return lines
 
 
+def extractMutliLinesStatement(lines, start, pattern, maxLinesCount=0):
+    """
+    """
+    oneLineStatement = ""
+    statementLineCount = 0
+    while (statementLineCount < maxLinesCount) or (maxLinesCount == 0):
+        if len(lines) <= start + statementLineCount:
+            break
+        oneLineStatement += lines[start + statementLineCount]
+        if oneLineStatement.endswith('\n'):
+            oneLineStatement = oneLineStatement[:-1]
+        statementLineCount += 1
+        statementMatch = pattern.match(oneLineStatement)
+        if statementMatch:
+            return statementLineCount, statementMatch
+
+    return None, None
+
+
 def processReplacement(lines):
     for i in range(len(lines)):
 
-        # Import statement
+        # Convert Import statement
 
         if importStatementPattern.match(lines[i]):
             lines[i] = "import org.slf4j.Logger;\nimport org.slf4j.LoggerFactory;\n"
 
-        # Declaration statement
-        declareStatementMatch = declareStatementPattern.match(lines[i])
-        if declareStatementMatch:
-            (indent, modifiers, logVariableName, classLogger) = declareStatementMatch.group('indent', 'modifiers', 'logVariableName', 'classLogger')
-            modifiers = modifiers.strip()
-            lines[i] = "{0} {1} Logger {2} = LoggerFactory.getLogger({3}.class);\n".format(indent, modifiers, logVariableName, classLogger)
+        # Convert Declaration statement
+
+        if lines[i].find("Logger") >= 0:
+            declareStatementLineCount, declareStatementMatch = extractMutliLinesStatement(lines, i, declareStatementPattern, 4)
+
+            if declareStatementMatch:
+                (indent, modifiers, logVariableName, classLogger) = declareStatementMatch.group('indent', 'modifiers', 'logVariableName', 'classLogger')
+                # could be better : should handle "private  \t\t...  static     final  \t  "
+                modifiers = modifiers.strip()
+                lines[i] = "{0}{1} Logger {2} = LoggerFactory.getLogger({3}.class);\n".format(indent, modifiers, logVariableName, classLogger)
+
+                # promote former statement trailing lines to nothing
+                for k in range(declareStatementLineCount - 1):
+                    lines[i + k + 1] = ""
+                # shift in the lines iteration
+                i += declareStatementLineCount - 1
+
 
 
 def writeLines(filePath, lines):
